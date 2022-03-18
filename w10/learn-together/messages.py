0fd47afe4ed83fdedd044ee61d35ac6761b3bbf8
Owen Mount <owenfletchfletchly@gmail.com>
2022-03-17 22:09:37 -0700

    w10 bug creation

---
+++ w10/learn-together/messages.py
@@ -0,0 +1,4 @@
+begin_messages = ["There is no room for cowardice on the journey that lies ahead.", "Perhaps you aren't the heroic type.", "The Dungeon is no place for fools.", "Heroes don't back down in the face of fear."]  # noqa
+
+monster_names = ["Dreadscream", "Wispstrike", "Boulderpest", "Emberserpent", "Helling", "Gutmonster", "Vilewraith", "Rustface"]  # noqa
+# Monster names generated from https://www.fantasynamegenerators.com/monster-names.php  # noqa
